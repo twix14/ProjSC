bf019177b29de4959ae0b9327e2c827306a1958f
danie <danie@10.101.228.52>
2017-02-23 12:28:48 +0000

    Servidor V 1.0 - basic

--- Proj1-SC/src/myGitServer.java
+++ Proj1-SC/src/myGitServer.java
@@ -1,6 +1,93 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class myGitServer {
-	public static void main(String args){
-		
+	public static void main(String[] args) {
+		System.out.println("servidor: main");
+		myGitServer server = new myGitServer();
+		server.startServer(Integer.parseInt(args[0]));
+	}
+
+	public void startServer (int socket){
+		File utilizadores = new File("utilizadores.txt");
+		ServerSocket sSoc = null;
+
+		try {
+			sSoc = new ServerSocket(socket);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+			System.exit(-1);
+		}
+
+		while(true) {
+			try {
+				Socket inSoc = sSoc.accept();
+				ServerThread newServerThread = new ServerThread(inSoc);
+				newServerThread.start();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+		//sSoc.close();
+	}
+	//Threads utilizadas para comunicacao com os clientes
+	class ServerThread extends Thread {
+
+		private Socket socket = null;
+
+		ServerThread(Socket inSoc) {
+			socket = inSoc;
+			System.out.println("thread do server para cada cliente");
+		}
+
+		public void run(){
+			try {
+				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+
+				String user = null;
+				String passwd;
+				int length = 0;
+				try {
+					user = (String)inStream.readObject();
+					passwd = (String)inStream.readObject();
+					System.out.println("thread: depois de receber a password e o user");
+				}catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+
+				//TODO: refazer
+				//este codigo apenas exemplifica a comunicacao entre o cliente e o servidor
+				//nao faz qualquer tipo de autenticacao
+				if (user.length() != 0){
+					outStream.writeObject(new Boolean(true));
+				}
+				else {
+					outStream.writeObject(new Boolean(false));
+				}
+				length = (int) inStream.readObject();
+				System.out.println(length);
+
+				
+				outStream.close();
+				inStream.close();
+
+				socket.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+
 	}
 }
