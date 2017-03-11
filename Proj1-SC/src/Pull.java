6f7f9687371f0651b895943cb8db61035716a4b6
danie <danie@DESKTOP-A5TR28O>
2017-03-11 19:17:51 +0000

    class pull

--- Proj1-SC/src/Pull.java
+++ Proj1-SC/src/Pull.java
@@ -1,11 +1,26 @@
 import java.io.Serializable;
+import java.util.List;
 
 public class Pull implements Serializable{
 
+	private String repRem;
+	List<Pair<String,Long>> files;
 	private static final long serialVersionUID = -857533466048543308L;
 
-	public Pull() {
-		// TODO Auto-generated constructor stub
+	public Pull(String repRem) {
+		this.repRem = repRem;
+	}
+
+	public List<Pair<String, Long>> getFiles() {
+		return files;
+	}
+	
+	public void addFiles(List<Pair<String,Long>> files){
+		this.files = files;
+	}
+	
+	public String getRep(){
+		return repRem;
 	}
 
 }
